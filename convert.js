c6dbae63b82e1488ef4187d8574a92fd815f000c
nmparfait <mbamu.developer@gmail.com>
2019-06-05 00:19:20 -0400

    fahrenheit temp converted

--- convert.js
+++ convert.js
@@ -1,2 +1,9 @@
 //Celsius = (Fahr-32)*5/9
 //Fahrenheit = Celcius *9/5 +32
+
+function convertToFahrenheit(temp){
+    let fahr = temp *9/5 +32;
+    console.log(`${temp} degrees in C is ${fahr} degrees in F`);
+}
+
+convertToFahrenheit(30);
